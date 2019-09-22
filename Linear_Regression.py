10885b5c2c086e413707d8da6088153c84842d7e
jamaliela <Jamalie@berea.edu>
2019-09-21 21:20:49 -0400

    cleaned up the code and now trying to add the file.

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -19,8 +19,9 @@ import seaborn as sb  # a library for high-level interface to draw statistical g
 from sklearn.model_selection import train_test_split  # library that splits arrays or matrices into random train and test subsets
 import matplotlib.patches as patches  # library to add patches
 
+
 # finding the Slope of linear regression line
-def Slope(a,b):
+def slope(a, b):
     n = len(a)
     two_sum = numpy.sum(a*b)
     sumX = numpy.sum(a)
@@ -29,33 +30,37 @@ def Slope(a,b):
     slope = (n*two_sum-sumX*sumY)/(n*sumX_2-(sumX)**2)
     return slope
 
-#Finding Intercept of linear regression line
-def Intercept(a,b):
-    intercept = numpy.mean(b)-Slope(a,b)*numpy.mean(a)
+
+# Finding Intercept of linear regression line
+def intercept(a, b):
+    intercept = numpy.mean(b)-slope(a,b)*numpy.mean(a)
     return intercept
 
-#predictions are made with the help of linear regression algorithm
-def Predictions(slope,x_input,intercept):
+
+# predictions are made with the help of linear regression algorithm
+def predictions(slope, x_input, intercept):
     predict = slope*x_input + intercept
     return predict
 
-#R-squared is regression metric
-def R_squared(predicted_values,test_values):
+
+# R-squared is regression metric
+def r_squared(predicted_values, test_values):
     f = predicted_values
     y = test_values
-    print(f,'\n\n',y)
-    #sum of squares
+    print(f, '\n\n', y)
+    # sum of squares
     ss_total = numpy.sum((y-numpy.mean(y))**2)
-    #regression sum
-    #ss_reg=numpy.sum((f-numpy.mean(y))**2)
-    #Residuals sum of squares
+    # regression sum
+    # ss_reg=numpy.sum((f-numpy.mean(y))**2)
+    # Residuals sum of squares
     ss_res = numpy.sum((y-f)**2)
-    #R-squared formula
+    # R-squared formula
     R_2 = 1-(ss_res/ss_total)
     return R_2
 
-#Finding Correlation Coefficient for the given X & Y values
-def correlation_coeff(predicted_values,test_values):
+
+# Finding Correlation Coefficient for the given X & Y values
+def correlation_coefficient(predicted_values,test_values):
     a = predicted_values
     b = test_values
     n = len(a)
@@ -67,8 +72,9 @@ def correlation_coeff(predicted_values,test_values):
     score = (n*two_sum-sumX*sumY)/numpy.sqrt((n*sumX_2-(sumX)**2)*(n*sumY_2-(sumY)**2))
     return score
 
-#Finding Covariance for the given X & Y values
-def Covariance(X,Y):
+
+# Finding Covariance for the given X & Y values
+def covariance(X, Y):
     a = X
     b = Y
     n = len(a)
@@ -76,73 +82,78 @@ def Covariance(X,Y):
     cov = two_sum/n-numpy.mean(a)*numpy.mean(b)
     return cov
 
-#Importing data(csv format) using pandas
-#Replace another dataset to make predictions
-dataset=pd.read_csv('Salary_Data.csv')
+
+def main():
+    """
+    The main function where all the other methods will be used for our data
+    """
+
+
+data = pd.read_csv('Agg_data.csv')  # Importing data using pandas library
 
 # Split-out validation dataset
-#knowing the dimenstions of data and making them READY for PREDICTIONS.
-array = dataset.values
+# knowing the dimenstions of data and making them READY for PREDICTIONS.
+array = data.values
 X = array[:,0]
-#print(X.shape)
-#X=X.reshape(1,-1).T
+# print(X.shape)
+# X=X.reshape(1,-1).T
 print(X.shape)
 Y = array[:,1]
 print(Y.shape)
 
-#To know the distribution of data let us plot box plot
-## 1
+# To know the distribution of data let us plot box plot
+# #1
 left = 0.1
 width = 0.8
-#fig=plot.figure()
-#fig,(ax1,ax2) = plot.subplots(nrows=2,ncols=1,sharex=False,sharey=True)
+# fig=plot.figure()
+# fig,(ax1,ax2) = plot.subplots(nrows=2,ncols=1,sharex=False,sharey=True)
 ax1 = plot.axes([left, 0.5, width, 0.45])
 ax1.boxplot(X)
 ax1.set_title('Box plot for X')
 plot.show()
-## 2
+# #2
 ax2 = plot.axes([left, 0.5, width, 0.45])
 ax2.boxplot(Y, '.-')
 ax2.set_title('Distribution of Y Data')
 plot.show()
 
-#Covariation in data
-print(Covariance(X,Y))
+# Covariation in data
+print(covariance(X, Y))
 
-#Dividing data into training and testing classes
+# Dividing data into training and testing classes
 test_size = 0.10
 seed = 7
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,test_size= test_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=test_size, random_state=seed)
 
-#finding intercepts of rgression line
-intercept=Intercept(X_train,Y_train)
-slope=Slope(X_train,Y_train)
-print(intercept,slope)
-predictions=Predictions(slope=slope,x_input=X_validation,intercept=intercept)
+# finding intercepts of rgression line
+intercept = intercept(X_train, Y_train)
+slope = slope(X_train, Y_train)
+print(intercept, slope)
+predictions = predictions(slope=slope, x_input=X_validation, intercept=intercept)
 print(predictions)
-print(R_squared(predicted_values=predictions,test_values=Y_validation))
-print(correlation_coeff(test_values=Y_validation,predicted_values=predictions))
+print(r_squared(predicted_values=predictions, test_values=Y_validation))
+print(correlation_coefficient(test_values=Y_validation, predicted_values=predictions))
 
-#Equation of Linear Regression
-y=slope*X+intercept
+# Equation of Linear Regression
+y = slope*X+intercept
 
-#plotting the linear regression function
-plot.scatter(X,Y,marker='^',color='k',alpha=0.55)
-plot.plot(X,y,color='R',linewidth=2)
+# plotting the linear regression function
+plot.scatter(X, Y, marker='^', color='k', alpha=0.55)
+plot.plot(X, y, color='R', linewidth=2)
 red_patch = patches.Patch(color='red', label='Regression Line')
-plot.legend(loc=0,handles=[red_patch])
+plot.legend(loc=0, handles=[red_patch])
 plot.title('Linear Regression Plot')
 plot.tight_layout(pad=2)
 plot.grid(False)
 plot.show()
 
-#Residual plots
+# Residual plots
 sb.set(style="whitegrid")
 # Make an example dataset with y ~ x
 rs = numpy.random.RandomState(7)
-#Plot the residuals after fitting a linear model
+# Plot the residuals after fitting a linear model
 sb.residplot(X, Y, lowess=True, color="r")
 plot.title('Residual Plot')
 plot.show()
 
-#--------------------------------------------------------------#
+
