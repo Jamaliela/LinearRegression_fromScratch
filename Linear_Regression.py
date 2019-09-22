08e082213e0361d0f49889aada3a8d9ed7c095a0
jamaliela <Jamalie@berea.edu>
2019-09-22 16:43:08 -0400

    it should be done. needs testing.

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -15,145 +15,71 @@
 import numpy   # library supporting large, multi-dimensional arrays and matrices.
 import matplotlib.pyplot as plot  # library for embedding plots
 import pandas as pd  # library to take data and creates a Python object with rows and columns
-import seaborn as sb  # a library for high-level interface to draw statistical graphics
-from sklearn.model_selection import train_test_split  # library that splits arrays or matrices into random train and test subsets
-import matplotlib.patches as patches  # library to add patches
 
 
-# finding the Slope of linear regression line
-def slope(a, b):
-    n = len(a)
-    two_sum = numpy.sum(a*b)
-    sumX = numpy.sum(a)
-    sumY = numpy.sum(b)
-    sumX_2 = numpy.sum(a**2)
-    slope = (n*two_sum-sumX*sumY)/(n*sumX_2-(sumX)**2)
-    return slope
+# Reading Data
+data = pd.read_csv('Agg_data.csv')
+print(data.shape)
+print(data.head())
 
+# Collecting X and Y
+X = data['Bid'].values
+Y = data['Market Price'].values
 
-# Finding Intercept of linear regression line
-def intercept(a, b):
-    intercept = numpy.mean(b)-slope(a,b)*numpy.mean(a)
-    return intercept
+# Mean X and Y to find B0 and B1
+mean_x = numpy.mean(X)
+mean_y = numpy.mean(Y)
 
+# Total number of values
+m = len(X)
 
-# predictions are made with the help of linear regression algorithm
-def predictions(slope, x_input, intercept):
-    predict = slope*x_input + intercept
-    return predict
+# Using the formula to calculate B1 and B0
+numerator = 0
+denominator = 0
+for i in range(m):
+    numerator += (X[i] - mean_x) * (Y[i] - mean_y)
+    denominator += (X[i] - mean_x) ** 2
+b1 = numerator / denominator
+b0 = mean_y - (b1 * mean_x)
 
+# Print coefficients
+print("This is B1:", b1, "and this is B0:", b0)
 
-# R-squared is regression metric
-def r_squared(predicted_values, test_values):
-    f = predicted_values
-    y = test_values
-    print(f, '\n\n', y)
-    # sum of squares
-    ss_total = numpy.sum((y-numpy.mean(y))**2)
-    # regression sum
-    # ss_reg=numpy.sum((f-numpy.mean(y))**2)
-    # Residuals sum of squares
-    ss_res = numpy.sum((y-f)**2)
-    # R-squared formula
-    R_2 = 1-(ss_res/ss_total)
-    return R_2
+# Plotting Values and Regression Line
 
+max_x = numpy.max(X) + 100
+min_x = numpy.min(X) - 100
 
-# Finding Correlation Coefficient for the given X & Y values
-def correlation_coefficient(predicted_values,test_values):
-    a = predicted_values
-    b = test_values
-    n = len(a)
-    two_sum = numpy.sum(a*b)
-    sumX = numpy.sum(a)
-    sumY = numpy.sum(b)
-    sumX_2 = numpy.sum(a**2)
-    sumY_2 = numpy.sum(b**2)
-    score = (n*two_sum-sumX*sumY)/numpy.sqrt((n*sumX_2-(sumX)**2)*(n*sumY_2-(sumY)**2))
-    return score
+# Calculating line values x and y
+x = numpy.linspace(min_x, max_x, 1000)
+y = b0 + b1 * x
 
+# Plotting Line
+plot.plot(x, y, color='#58b970', label='Regression Line')
+# Plotting Scatter Points
+plot.scatter(X, Y, c='#ef5423', label='Scatter Plot')
 
-# Finding Covariance for the given X & Y values
-def covariance(X, Y):
-    a = X
-    b = Y
-    n = len(a)
-    two_sum = numpy.sum(a*b)
-    cov = two_sum/n-numpy.mean(a)*numpy.mean(b)
-    return cov
-
-
-def main():
-    """
-    The main function where all the other methods will be used for our data
-    """
-
-
-data = pd.read_csv('Agg_data.csv')  # Importing data using pandas library
-
-# Split-out validation dataset
-# knowing the dimenstions of data and making them READY for PREDICTIONS.
-array = data.values
-X = array[:,0]
-# print(X.shape)
-# X=X.reshape(1,-1).T
-print(X.shape)
-Y = array[:,1]
-print(Y.shape)
-
-# To know the distribution of data let us plot box plot
-# #1
-left = 0.1
-width = 0.8
-# fig=plot.figure()
-# fig,(ax1,ax2) = plot.subplots(nrows=2,ncols=1,sharex=False,sharey=True)
-ax1 = plot.axes([left, 0.5, width, 0.45])
-ax1.boxplot(X)
-ax1.set_title('Box plot for X')
-plot.show()
-# #2
-ax2 = plot.axes([left, 0.5, width, 0.45])
-ax2.boxplot(Y, '.-')
-ax2.set_title('Distribution of Y Data')
+plot.xlabel('Bid')
+plot.ylabel('Market Price')
+plot.legend()
 plot.show()
 
-# Covariation in data
-print(covariance(X, Y))
+# Calculating Root Mean Squares Error
+RMSE = 0
+for i in range(m):
+    y_prediction = b0 + b1 * X[i]
+    RMSE += (Y[i] - y_prediction) ** 2
+RMSE = numpy.sqrt(RMSE/m)
+print("This is Root Mean Square Error", RMSE)
 
-# Dividing data into training and testing classes
-test_size = 0.10
-seed = 7
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=test_size, random_state=seed)
-
-# finding intercepts of rgression line
-intercept = intercept(X_train, Y_train)
-slope = slope(X_train, Y_train)
-print(intercept, slope)
-predictions = predictions(slope=slope, x_input=X_validation, intercept=intercept)
-print(predictions)
-print(r_squared(predicted_values=predictions, test_values=Y_validation))
-print(correlation_coefficient(test_values=Y_validation, predicted_values=predictions))
-
-# Equation of Linear Regression
-y = slope*X+intercept
-
-# plotting the linear regression function
-plot.scatter(X, Y, marker='^', color='k', alpha=0.55)
-plot.plot(X, y, color='R', linewidth=2)
-red_patch = patches.Patch(color='red', label='Regression Line')
-plot.legend(loc=0, handles=[red_patch])
-plot.title('Linear Regression Plot')
-plot.tight_layout(pad=2)
-plot.grid(False)
-plot.show()
-
-# Residual plots
-sb.set(style="whitegrid")
-# Make an example dataset with y ~ x
-rs = numpy.random.RandomState(7)
-# Plot the residuals after fitting a linear model
-sb.residplot(X, Y, lowess=True, color="r")
-plot.title('Residual Plot')
-plot.show()
+# calculating coefficient determination
+SST = 0
+SSR = 0
+for i in range(m):
+    y_prediction = b0 + b1 * X[i]
+    SST += (Y[i] - mean_y) ** 2
+    SSR += (Y[i] - y_prediction) ** 2
+r2 = 1 - (SSR/SST)
+print("This is the coefficient of determination", r2)
 
 
